6fdb94246fefaac306c2fd9c0527b270aa686642
Matias Baez <matiascaneda3@gmail.com>
2023-09-14 14:35:49 -0300

    responsive

--- js/index.js
+++ js/index.js
@@ -19,19 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 temperatureElement.textContent = `${Math.round(data.list[0].main.temp)}°C`;
 
                 for (let i = 1; i <= 5; i++) {
-                const dayContainer = dayContainers[i - 1];
-                const dayData = data.list[i * 8];
-
-                if (dayData) {
-                    const daySmall = dayContainer.querySelector('.day-small');
-                    const temp = dayContainer.querySelector('.temp');
-                    const icon = dayContainer.querySelector('.icon');
-
-                    daySmall.textContent = new Date(dayData.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' });
-                    temp.textContent = `${Math.round(dayData.main.temp)}°C`;
-                    icon.src = `https://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`;
+                    const dayContainer = dayContainers[i - 1];
+                    const dayData = data.list[i * 8 - 1]; // Use i * 8 - 1 to get data for the correct day
+                
+                    if (dayData) {
+                        const daySmall = dayContainer.querySelector('.day-small');
+                        const temp = dayContainer.querySelector('.temp');
+                        const icon = dayContainer.querySelector('.icon');
+                
+                        daySmall.textContent = new Date(dayData.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' });
+                        temp.textContent = `${Math.round(dayData.main.temp)}°C`;
+                        icon.src = `https://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`;
+                    }
                 }
-                }
+                
 
                 glassCardContainer.classList.add('expanded');
 
